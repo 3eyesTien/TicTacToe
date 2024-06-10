56eb3d44aa7c8112302cf5951e3d96681341d619
Tien Ho <7801322+3eyesTien@users.noreply.github.com>
2024-06-09 21:13:08 -0700

    Beginning of board creation
    Beginning of board creation. Top half

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -13,5 +13,31 @@ bool checkTie(char* spaces);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char spaces[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+    char player = 'X';
+    char computer = 'O';
+    bool gameActive;
+
+    drawBoard(spaces); //When an array is passed into a function, it decays into a pointer due to how we defined the function
+    return 0;
+}
+
+void drawBoard(char* spaces) {
+    std::cout << "     |     |     " << "\n";
+    std::cout << "  " << spaces[0] << "  |  " << spaces[1] << "  |  " << "  " << spaces[2] << "\n";
+    std::cout << "_____|_____|_____" << "\n";
+}
+void playerMove(char* spaces, char player) {
+
+}
+void computerMove(char* spaces, char computer) {
+
+}
+bool checkWinner(char* spaces, char player, char computer) {
+
+    return 0;
+}
+bool checkTie(char* spaces) {
+
+    return 0;
 }
